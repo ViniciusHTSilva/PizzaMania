544d039673fae460bd2517681d31731c8535bcd4
Vinicius Henrique <88394501+ViniciusHTSilva@users.noreply.github.com>
2021-08-12 03:13:32 -0300

    parte 1
    desenvolvi o menu coloquei a parte do ifood em botão assim como whatsaap para poder fazer pedidos próxima adcionar mais conteúdo e colocar a pagina de cardápio

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+function menu(){
+document.getElementById('Abm').style.width = '250px';
+document.getElementById('conteudo').style.marginleft = '250px';
+}
+
+function fechar(){
+    document.getElementById('Abm').style.width = '0px';
+    document.getElementById('conteudo').style.marginleft = '0px';
+   
+}
